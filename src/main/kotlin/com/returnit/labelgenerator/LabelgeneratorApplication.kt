3a45c1be1ca4a07c905185b7f72abdf6cbee7e51
jroell <roelljr@gmail.com>
2018-02-15 14:55:56 -0500

    First Commit

---
+++ src/main/kotlin/com/returnit/labelgenerator/LabelgeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.returnit.labelgenerator
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LabelgeneratorApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(LabelgeneratorApplication::class.java, *args)
+}
